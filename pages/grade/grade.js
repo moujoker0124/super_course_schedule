585483093d168536322a5211a97bff43feef8f5c
mou_joker <143597331+moujoker0124@users.noreply.github.com>
2023-09-08 13:50:44 +0800

    格式化

--- pages/grade/grade.js
+++ pages/grade/grade.js
@@ -21,59 +21,61 @@ Page({
     })
   },
 
-onShow:function(){
-  this.getGrade();
-},
+  onShow: function () {
+    this.getGrade();
+  },
 
-getGrade:function(){
-if(!wx.getStorageSync('isLogin')){
-      wx.showModal({title: '加载失败', content: '请先登录！', showCancel: false, success: function(res) {
-        wx.switchTab({
-          url:'../../pages/index/index',
-            success:function(){
+  getGrade: function () {
+    if (!wx.getStorageSync('isLogin')) {
+      wx.showModal({
+        title: '加载失败', content: '请先登录！', showCancel: false, success: function (res) {
+          wx.switchTab({
+            url: '../../pages/index/index',
+            success: function () {
               console.log("called switchtab.");
+            }
+          });
         }
       });
-    }});
-  }else{
-    //已登录
-    wx.showToast({title: '正在加载成绩', icon: 'loading', duration: 10000});
-    if(wx.getStorageSync('grade_cache')){
-      wx.hideToast();
-      this.setData({
-        grade_empty: false,
-        list: wx.getStorageSync('grade_cache'),
-      });
-    }else{
-      var $this = this;
-      wx.request({
-      url: 'https://fupengfei.s1.natapp.cc/grade_admin/get_all_grade',
-      data: {
-      'username':wx.getStorageSync('userName'),
-      'password':wx.getStorageSync('userPassword'),
-      },
-      method: 'GET',
-      dataType: 'json',
-      success: function(res){
+    } else {
+      //已登录
+      wx.showToast({ title: '正在加载成绩', icon: 'loading', duration: 10000 });
+      if (wx.getStorageSync('grade_cache')) {
         wx.hideToast();
-      if(res.statusCode == 200 && res.data.code == 200){
-        $this.setData({
-          grade_empty:false,
-          list:res.data.data
+        this.setData({
+          grade_empty: false,
+          list: wx.getStorageSync('grade_cache'),
         });
-        wx.setStorageSync('grade_cache', res.data.data) //缓存成绩
-        console.log(res.data.data);
-      }else{
-        wx.showModal({title: '加载失败', content: '请检查网络设置！', showCancel: false});
+      } else {
+        var $this = this;
+        wx.request({
+          url: 'https://fupengfei.s1.natapp.cc/grade_admin/get_all_grade',
+          data: {
+            'username': wx.getStorageSync('userName'),
+            'password': wx.getStorageSync('userPassword'),
+          },
+          method: 'GET',
+          dataType: 'json',
+          success: function (res) {
+            wx.hideToast();
+            if (res.statusCode == 200 && res.data.code == 200) {
+              $this.setData({
+                grade_empty: false,
+                list: res.data.data
+              });
+              wx.setStorageSync('grade_cache', res.data.data) //缓存成绩
+              console.log(res.data.data);
+            } else {
+              wx.showModal({ title: '加载失败', content: '请检查网络设置！', showCancel: false });
+            }
+          },
+          fail: function () {
+            wx.hideToast();
+            wx.showModal({ title: '加载失败', content: '请检查网络设置！', showCancel: false });
+          },
+        })
       }
-      },
-      fail: function() {
-        wx.hideToast();
-        wx.showModal({title: '加载失败', content: '请检查网络设置！', showCancel: false});
-      },
-      })
     }
-  }
-},
-  
+  },
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,105 +3,105 @@ Page({
     loadingHidden: true,
     modalHidden: true,
     modalContent: '',
-    userName:'',
-    userPassword:'',
-    isLogin:false,
-    user:'',
-    successHidden:false,
-    loadingHidden:false,
-    logoutHidden:false
+    userName: '',
+    userPassword: '',
+    isLogin: false,
+    user: '',
+    successHidden: false,
+    loadingHidden: false,
+    logoutHidden: false
   },
 
-formSubmit:function(e){
- var $this = this;
- console.log(e.detail.value);
- wx.showToast({title: '正在登陆', icon: 'loading', duration: 10000});
-  //获得表单数据
-var objData = e.detail.value;
-if(objData.userName && objData.userPassword){
-   // 同步方式存储表单数据
-   wx.setStorageSync('userName', objData.userName);
-   wx.setStorageSync('userPassword', objData.userPassword);
-   wx.setStorageSync('remind', objData.remind);
-   //请求教务系统
-   wx.request({
-    url: 'https://fupengfei.s1.natapp.cc/personal_admin/check_user',
-    data: {
-    'username':objData.userName,
-    'password':objData.userPassword,
-    },
-    method: 'GET',
-    dataType: 'json',
-    success: function(res){
-    wx.hideToast();
-    if(res.statusCode == 200 && res.data.code == 200){
-       wx.setStorageSync('isLogin', true);
-       $this.setData({isLogin: true,user: res.data.data});
-        wx.setStorageSync('user', res.data.data);//登录者的名字 树洞页面要用到
-        wx.showToast({title: '已登录', icon: 'success', duration: 1500});
-      }else{
+  formSubmit: function (e) {
+    var $this = this;
+    console.log(e.detail.value);
+    wx.showToast({ title: '正在登陆', icon: 'loading', duration: 10000 });
+    //获得表单数据
+    var objData = e.detail.value;
+    if (objData.userName && objData.userPassword) {
+      // 同步方式存储表单数据
+      wx.setStorageSync('userName', objData.userName);
+      wx.setStorageSync('userPassword', objData.userPassword);
+      wx.setStorageSync('remind', objData.remind);
+      //请求教务系统
+      wx.request({
+        url: 'https://fupengfei.s1.natapp.cc/personal_admin/check_user',
+        data: {
+          'username': objData.userName,
+          'password': objData.userPassword,
+        },
+        method: 'GET',
+        dataType: 'json',
+        success: function (res) {
+          wx.hideToast();
+          if (res.statusCode == 200 && res.data.code == 200) {
+            wx.setStorageSync('isLogin', true);
+            $this.setData({ isLogin: true, user: res.data.data });
+            wx.setStorageSync('user', res.data.data);//登录者的名字 树洞页面要用到
+            wx.showToast({ title: '已登录', icon: 'success', duration: 1500 });
+          } else {
+            wx.setStorageSync('isLogin', false);
+            $this.setData({ isLogin: false });
+            wx.showModal({ title: '登录失败', content: '账号或密码错误!', showCancel: false });
+          }
+        },
+        fail: function () {
+          wx.hideToast();
           wx.setStorageSync('isLogin', false);
-          $this.setData({isLogin: false});
-          wx.showModal({title: '登录失败', content: '账号或密码错误!', showCancel: false});
-     }
-    },
-    fail: function() {
-    wx.hideToast();
-    wx.setStorageSync('isLogin', false);
-        $this.setData({isLogin: false});
-        wx.showModal({title: '登录失败', content: '请检查网络设置!', showCancel: false});
-    },
-    })
-   }else{
-       wx.hideToast();
-       wx.setStorageSync('isLogin', false);
-        $this.setData({isLogin: false});
-        wx.showModal({title: '登录失败', content: '请输入账号密码!', showCancel: false});
-   }
-},
+          $this.setData({ isLogin: false });
+          wx.showModal({ title: '登录失败', content: '请检查网络设置!', showCancel: false });
+        },
+      })
+    } else {
+      wx.hideToast();
+      wx.setStorageSync('isLogin', false);
+      $this.setData({ isLogin: false });
+      wx.showModal({ title: '登录失败', content: '请输入账号密码!', showCancel: false });
+    }
+  },
 
-logout:function(e){
-    wx.showToast({title: '已退出', icon: 'success', duration: 1500});
+  logout: function (e) {
+    wx.showToast({ title: '已退出', icon: 'success', duration: 1500 });
     //销毁用户数据
     this.setData({
-        isLogin: false,
-        userName:'',
-        userPassword:'',
+      isLogin: false,
+      userName: '',
+      userPassword: '',
     });
     wx.clearStorage();
-},
+  },
 
- //加载完后，处理事件 
- // 如果有本地数据，则直接显示
- onLoad:function(options){
-  //获取本地数据
+  //加载完后，处理事件 
+  // 如果有本地数据，则直接显示
+  onLoad: function (options) {
+    //获取本地数据
     var userName = wx.getStorageSync('userName');
     var userPassword = wx.getStorageSync('userPassword');
 
-    if(userName){
-        this.setData({userName: userName});
+    if (userName) {
+      this.setData({ userName: userName });
     }
-    if(userPassword){
-        this.setData({userPassword: userPassword});
+    if (userPassword) {
+      this.setData({ userPassword: userPassword });
     }
- 
- },
- onReady:function(){
-  // 页面渲染完成
- },
- onShow:function(){
-  // 页面显示
- },
- onHide:function(){
-  // 页面隐藏
- },
- onUnload:function(){
-     // 页面关闭
-     console.log('unload');
-     var remind = wx.getStorageSync('remind');
-     if(!remind){
-        wx.removeStorageSync('userName');
-        wx.removeStorageSync('userPassword');
-     }
- }
+
+  },
+  onReady: function () {
+    // 页面渲染完成
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+    console.log('unload');
+    var remind = wx.getStorageSync('remind');
+    if (!remind) {
+      wx.removeStorageSync('userName');
+      wx.removeStorageSync('userPassword');
+    }
+  }
 })
